4a6537e73e52e0886b12510e119faeb35e5d5ea9
Paul Rizzo <prizzo@levelsbeyond.com>
2019-04-07 10:23:22 -0600

    updated camera

--- routes/camera.js
+++ routes/camera.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('camera', {
-    title: 'Garden Bot Webcam'
-  });
+router.get('/', function (req, res, next) {
+    res.render('camera', {
+        title: 'Garden Bot',
+        pagetitle: 'Webcam'
+    });
 });
 
 module.exports = router;
